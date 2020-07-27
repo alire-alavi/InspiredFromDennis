f8d7afac4bf397952d4da6032f8c56156dd7f8ff
Ar-Raz <alirezaalavirazavi@yahoo.com>
2020-07-27 00:34:10 -0400

    modified dashboard

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+GENDER_CHOICE = (
+    ('آقای','آقای'),
+    ('خانم', 'خانم')
+)
+
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=True)
     phone = models.CharField(max_length=15, null=True)
     email = models.CharField(max_length=100, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    province = models.CharField(max_length=32, blank=True, null=True)
+    city = models.CharField(max_length=32, blank=True, null=True)
+    GmBH = models.CharField(max_length=64, blank=True, null=True)
+    forname = models.CharField(max_length=64, blank=True, null=True)
+    nachname = models.CharField(max_length=64, blank=True, null=True)
+    gender = models.CharField(max_length=6, blank=True , null=True, choices=GENDER_CHOICE)
+
 
     def __str__(self):
         return self.name
@@ -46,7 +58,7 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    note = models.CharField(max_length=1000, null=True)
+    note = models.TextField(blank=True, null=True)
 
     def __str__(self):
 	    return f"order of {self.product.name}"
